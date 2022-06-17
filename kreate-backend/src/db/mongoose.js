d3886ca60000b938bcc118d7d5fc2c3c5a8fd415
khan Waseem <47791198+khnwaseem@users.noreply.github.com>
2022-06-17 07:54:04 +0530

    Update mongoose.js

--- kreate-backend/src/db/mongoose.js
+++ kreate-backend/src/db/mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 try {
     // Connect to the MongoDB cluster
      mongoose.connect(
-        "mongodb+srv://khanwaseem:Waseem$111@cluster0.fcxohmn.mongodb.net/?retryWrites=true&w=majority",
+        "",
       { useNewUrlParser: true, useUnifiedTopology: true },
       () => console.log(" Mongoose is connected")
     );
